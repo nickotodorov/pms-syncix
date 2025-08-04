85fc15b3f5b77f8488ca158a34c6a3f11dbbb02b
Nikolay Todorov <ntodorov@despark.com>
2025-08-04 14:35:24 +0300

    fix: use correct model

--- app/Repositories/RoomRepository.php
+++ app/Repositories/RoomRepository.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Repositories;
 
 use App\Models\Room;
-use App\Models\RoomType;
 
 class RoomRepository
 {
     public function updateOrCreate(array $roomData, int $roomTypeId): Room
     {
         $syncHash = md5(json_encode($roomData));
-        $model = RoomType::find($roomData['id']);
+        $model = Room::find($roomData['id']);
 
         if ($model && $model->sync_hash === $syncHash) {
             return $model;
